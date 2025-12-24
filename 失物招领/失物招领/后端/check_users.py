d5323c24bef5a9d663c22d9841b64a5aa36f42eb
Li Ziquan <482944803@qq.com>
2025-12-24 16:24:13 +0800

    Delete 失物招领/失物招领 directory

--- 失物招领/失物招领/后端/check_users.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.models import User
-
-app = create_app()
-with app.app_context():
-    users = User.query.all()
-    print(f'Total users: {len(users)}')
-    for user in users:
-        print(f'User: {user.username}, Password starts with: {user.password[:10]}...')
